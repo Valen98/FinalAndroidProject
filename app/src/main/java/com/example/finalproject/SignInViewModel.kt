d69def2ac7c30c237d7b22a81ceb78f12350a842
mbielawski <mbielawski21@ubishops.ca>
2023-12-17 16:54:29 -0500

    asd

--- app/src/main/java/com/example/finalproject/SignInViewModel.kt
+++ app/src/main/java/com/example/finalproject/SignInViewModel.kt
@@ -1,6 +1,6 @@
 package com.example.finalproject
 
-//notes
+//notes but extra notes
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
